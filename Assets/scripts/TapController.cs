ff0a4af81fbb0827dab6ea020753e58e42341066
DESKTOP-MRC2IME\A-23 <DESKTOP-MRC2IME\A-23>
2022-08-10 19:55:06 +0800

    ubah string di TapController.cd(89,29)

--- Assets/scripts/TapController.cs
+++ Assets/scripts/TapController.cs
@@ -80,7 +80,7 @@ public class TapController : MonoBehaviour {
 			scoreAudio.Play();
 		}
 
-		if (col.gameObject.tag == "DeadZones") {
+		if (col.gameObject.tag == "DeadZone") {
 		
 			rigidbody.simulated = false;
 			OnPlayerDied ();
